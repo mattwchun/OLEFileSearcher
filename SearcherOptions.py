f57a5db4628829f1f415aa4a09c594867b48ba59
U-CZNET\M6MACHUN <M6MACHUN@M601Q010.cznet.zeiss.org>
2016-09-06 11:33:25 -0700

    Filled out SearcherOptions method stubs

--- SearcherOptions.py
+++ SearcherOptions.py
@@ -1,15 +1,27 @@
 from Tkinter import Tk
 from tkFileDialog import askopenfilename
+import olefile
+import SearcherModel
+
+ole = None
 
 def NewFile():
     Tk().withdraw()
     filename = askopenfilename()
+    ole = olefile.OleFileIO(filename)
     print filename
     print 'NewFile()'
     return True
 
 def Search():
+    if ole == None:
+        print 'No file has been selected'
+    else:
+        value = raw_input("Enter search value: ")
+        SearcherModel.search(ole, value)
     print 'Search()'
+    return True
 
 def Exit():
     print 'Exit()'
+    return False
